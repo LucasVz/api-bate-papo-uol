801fe797d10c22bea3b27f29227e9747229ffc50
LucasVz <lucastadeuvaz@gmail.com>
2022-01-31 02:37:40 -0300

    add post messages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { MongoClient, ObjectId} from 'mongodb';
 import dotenv from 'dotenv'
 import dayjs from "dayjs";
+import joi from 'joi'
 
 dotenv.config();
 
@@ -16,19 +17,38 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let name;
+const participantSchema = joi.object({
+    name: joi.string().required()
+});
 
 app.post('/participants', async (req,res) =>{
+    const participant = req.body;
+    const time = dayjs().format('hh:mm:ss');
+    const validation = participantSchema.validate(participant, { abortEarly: true });
+    let timeInMs = Date.now();
+    const message = {from: req.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: time}
+    const participantFormat =  {name: participant.name, lastStatus: timeInMs}
+    if (validation.error) {
+        res.sendStatus(422);
+        return;
+    }
+
+    let findParticipant = await db.collection('participants').find(participant).toArray()
+
+    console.log(findParticipant);
+    if (findParticipant.length > 0){
+        res.sendStatus(409);
+        return;
+    }
+
     try{
-        const participant = req.body;
-        const time = dayjs().format('hh:mm:ss');
-        const message = {from: req.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: time}
-        await db.collection('participants').insertOne(participant);
+        await db.collection('participants').insertOne(participantFormat);
         await db.collection('messages').insertOne(message);
         res.status(201).send("OK");
     }
     catch (error) {
-        res.sendStatus(500)
+        console.log(error);
+        res.sendStatus(500);
     }
 });
 
@@ -41,6 +61,25 @@ app.get('/participants', async (req,res) =>{
         res.sendStatus(500)
     }
 });
+
+app.post('/messages', async (req,res) =>{
+    const message = req.body;
+    const user = req.headers.user;
+    const time = dayjs().format('hh:mm:ss');
+    //const validation = participantSchema.validate(participant, { abortEarly: true });
+    let timeInMs = Date.now();
+    const messageFormat = {from: user, to: message.to, text: message.text, type: message.type, time: time}
+
+    try{
+        await db.collection('messages').insertOne(messageFormat);
+        res.status(201).send("OK");
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
 
 app.get('/messages', async (req,res) =>{
     try{
